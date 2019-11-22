c2a145bf1a7cf033e982c2e277f2dd58b516018a
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-22 21:10:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,11 +5,22 @@ class Triangle
     @side_three = side_three
   end
 
-  def kind
-    :equilateral
-    :isosceles
-    :scalene
+  def kind()
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
+      raise TriangleError
+    elsif @side_one + @side_two <= @side_three || @side_one + @side_three <= @side_two || @side_two + @side_three <= @side_one
+      raise TriangleError
+    else
+      if @side_one == @side_two && @side_two == @side_three
+        :equilateral
+    elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
+        :isosceles
+    elsif @side_one != @side_two && @side_two != @side_three && @side_one != @side_three
+        :scalene
+      end
+    end
   end
+end
 
   class TriangleError < StandardError
 
